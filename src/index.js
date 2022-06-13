2bbb6eaf7d16d52f329fbe2b6107dab27cb32df4
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-06-14 02:26:06 +0300

    part 02-task 01

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
-const input = document.querySelector("#search-box");
+const input = document.querySelector("input#search-box");
 
 input.addEventListener('input', debounce((event) => { 
     event.preventDefault();
-    const name = event.target.value;
+    const name = event.target.value.trim();
+    if (name!=="") {
+        fetchCountries(name);        
+    }
     
-    fetchCountries(name); 
 }, DEBOUNCE_DELAY)
 )
 
 //убрать в отдельный файл и сделать именованный экспорт
 function fetchCountries(name) {
-    fetch(`https://restcountries.com/v2/name/${name}`)
+    fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`)
         
         .then(response => {
             console.log(response);
-            // if (!response.ok) {
-            //  throw new Error(response.status);
-            // } 
+            if (!response.ok) {
+             throw new Error(response.status);
+            } 
             return response.json();
         })
-        .then(country => { 
-            console.log(country);
+        .then(countries => {
+
+            console.log(countries.length);
         })
         .catch(error => { 
-            console.log(`Error`);
+            Notify.failure("Oops, there is no country with that name");
         })
 }
 
